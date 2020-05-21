470c9fedb24e50e95d561991bab2d478e27990fa
Subhasish <subhasish469@gmail.com>
2020-05-21 20:26:14 +0530

    user can select and add the photo to the firebase server

--- app/src/main/java/com/example/firebasesocialmediaapp/SocialMediaActivity.java
+++ app/src/main/java/com/example/firebasesocialmediaapp/SocialMediaActivity.java
@@ -1,23 +1,80 @@
 package com.example.firebasesocialmediaapp;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
+import java.util.UUID;
 
 public class SocialMediaActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+    private ImageView postImageView;
+    private Button btnCreatePost;
+    private EditText edtDes;
+    private ListView usersListView;
+    private Bitmap bitmap;
+    private String imageIdentifier;
+    private ArrayList<String> usernames;
+    private ArrayAdapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_social_media);
         mAuth = FirebaseAuth.getInstance();
+        postImageView=findViewById(R.id.postImageView);
+        btnCreatePost=findViewById(R.id.btnCreatePost);
+        edtDes=findViewById(R.id.edtDes);
+        usersListView=findViewById(R.id.usersListView);
+
+        postImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                selectImage();
+
+            }
+        });
+
+        btnCreatePost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                uploadImageToServer();
+
+            }
+        });
     }
 
 
@@ -56,5 +113,74 @@ public class SocialMediaActivity extends AppCompatActivity {
         mAuth.signOut();
         finish();
 
+    }
+    private void selectImage(){
+        if(Build.VERSION.SDK_INT<23){
+            Intent intent=new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(intent,1000);
+
+        }else if(Build.VERSION.SDK_INT>=23)
+            if(ContextCompat.checkSelfPermission(this,Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
+                requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},1000);
+            }else {
+                Intent intent=new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent,1000);
+            }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode==1000&&grantResults[0]==PackageManager.PERMISSION_GRANTED){
+            selectImage();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==1000&&resultCode==RESULT_OK&&data!=null){
+            Uri chosenImageData=data.getData();
+
+            try {
+                bitmap=MediaStore.Images.Media.getBitmap(this.getContentResolver(),chosenImageData);
+                postImageView.setImageBitmap(bitmap);
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void uploadImageToServer(){
+
+        if (bitmap!=null) {
+            // Get the data from an ImageView as bytes
+            postImageView.setDrawingCacheEnabled(true);
+            postImageView.buildDrawingCache();
+            //Bitmap bitmap = ((BitmapDrawable) postImageView.getDrawable()).getBitmap();
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
+            byte[] data = baos.toByteArray();
+
+            imageIdentifier = UUID.randomUUID() + ".png"; //assigning unique value to the images
+
+            UploadTask uploadTask = FirebaseStorage.getInstance().getReference().child("my_images").child(imageIdentifier).putBytes(data);
+            uploadTask.addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                    // Handle unsuccessful uploads
+                    Toast.makeText(getApplicationContext(), exception.toString(), Toast.LENGTH_SHORT).show();
+                }
+            }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    // taskSnapshot.getMetadata() contains file metadata such as size, content-type, etc.
+                    // ...
+                    Toast.makeText(getApplicationContext(), "UPLOAD SUCCESSFUL", Toast.LENGTH_SHORT).show();
+                    edtDes.setVisibility(View.VISIBLE);
+                }
+            });
+        }
     }
 }
